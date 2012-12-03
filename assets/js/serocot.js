38c4134df6c41a2b1d070991eb440954c56eeded
serocot <no>
2012-12-03 13:51:21 +0000

    yay, changing captchas

--- assets/js/serocot.js
+++ assets/js/serocot.js
@@ -1,18 +1,23 @@
 function getNewCaptcha() {
 
-	// yes
-	alert('beep');
+    // the three samples is just temporary
 
-}
+    var cnum = Math.floor(Math.random() * 3) + 1;
+
+    $(document).ready(function(){
+        $('.captcha').attr('src', '../assets/img/sample_' + cnum + '.jpg');
+    });
+
+};
 
 $(window).load(function() {
 
-	$('.captcha').click(function(){
-		getNewCaptcha();
-	})
+    $('.captcha').click(function(){
+        getNewCaptcha();
+    });
 
-	$('.captcha').hover(function() {
-    	$(this).css('cursor','pointer');
-	})
+    $('.captcha').hover(function() {
+        $(this).css('cursor', 'pointer');
+    });
 
-})
+});
